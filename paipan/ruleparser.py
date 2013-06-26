3743b744542c55080b79eb4326a42e665bb12c17
opensuse <opensuse@linux-1pzc.(none)>
2013-06-26 13:24:10 +0800

    v0.2 extract rule parse into one class

---
+++ paipan/ruleparser.py
@@ -0,0 +1,128 @@
+import ply.lex as lex
+import ply.yacc as yacc
+import logging
+
+class RuleParser(object):
+    # tokens
+    reserved = (
+            'TARGET','NAME','INPUT','OUTPUT','DATA')
+    tokens = reserved + (
+        'COMMENT','NUMBER','ID','SCONST',
+        )
+
+    # constructor
+    def __init__(self,loglevel=logging.CRITICAL):
+        self.initlog(loglevel)
+        self._reserved_map={}
+        for r in RuleParser.reserved:
+            self._reserved_map[r.upper()] = r
+        #self._lexer=lex.lex(module=self)
+        lex.lex(module=self)
+        yacc.yacc(module=self)
+
+    # public functions
+    def parse(self,fname,encode):
+        # load rule file
+        fh=open(fname,'rU')
+        lines=[]
+        for line in fh.readlines():
+            lines.append(line.strip().decode(encode).encode('utf8'))
+        fh.close()
+        self.init()
+        self._parse("\n".join(lines))
+    def results(self):
+        return tuple(self._result)
+
+    # private functions
+    def initlog(self,loglevel):
+        self._log=logging.getLogger('RuleParser')
+        self._log.setLevel(loglevel)
+        ch=logging.StreamHandler()
+        ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self._log.addHandler(ch)
+    def init(self):
+        self._result=[]
+    def initstate(self):
+        self._pinfo={}
+    def _parse(self,statements):
+        self.initstate()
+        yacc.parse(statements)
+        #self._lexer.input(statements)
+        #while True:
+            #tok=self._lexer.token()
+            #if not tok:
+                #break
+            #self._log.debug(tok)
+
+    # lexer
+    def t_ID(self,t):
+        r'[A-Za-z_][\w_]*'
+        t.type = self._reserved_map.get(t.value.upper(),"ID")
+        return t
+    def t_COMMENT(self,t):
+        r'\#.*'
+        return t
+    def t_NUMBER(self,t):
+        r'\d+\.?\d*'
+        t.value=int(t.value)
+        return t
+    def t_SCONST(self,t):
+        r'".*?"'
+        t.value=t.value[1:-1]
+        return t
+    def t_newline(self,t):
+        r'\n+'
+        t.lexer.lineno+=len(t.value)
+    t_ignore  =': \t\x0c,'
+    def t_error(self,t):
+        self._log.critical("T_ERROR: %s" % repr(t.value[0]))
+        t.lexer.skip(1)
+
+    # parser
+    def p_rulefile(self,p):
+        '''rulefile : item
+                    | rulefile item'''
+    def p_item(self,p):
+        '''item : target input output data
+                | target name input output data
+                | COMMENT'''
+        if p[1]=='PTARGET':
+            self._result.append(self._pinfo)
+            self._log.debug(repr(self._pinfo))
+        self.initstate()
+    def p_target(self,p):
+        '''target : TARGET ID'''
+        p[0]='PTARGET'
+        self._pinfo[p[0]]=p[2]
+    def p_name(self,p):
+        '''name : NAME SCONST'''
+        p[0]='PNAME'
+        self._pinfo[p[0]]=p[2]
+    def p_input(self,p):
+        '''input : INPUT ID
+                 | input ID'''
+        p[0]='PINPUT'
+        if p[1]!='PINPUT':
+            self._pinfo[p[0]]=[]
+        self._pinfo[p[0]].append(p[2])
+    def p_output(self,p):
+        '''output : OUTPUT NUMBER
+                  | output NUMBER'''
+        p[0]='POUTPUT'
+        if p[1]!='POUTPUT':
+            self._pinfo[p[0]]=[]
+        self._pinfo[p[0]].append(p[2])
+    def p_data(self,p):
+        '''data : DATA NUMBER
+                | data NUMBER'''
+        p[0]='PDATA'
+        if p[1]!='PDATA':
+            self._pinfo[p[0]]=[]
+        self._pinfo[p[0]].append(p[2])
+    def p_error(self,p):
+        self._log.critical("P_ERROR: "+str(p))
+
+if __name__ == '__main__':
+    rp=RuleParser(logging.DEBUG)
+    rp.parse('ziwei_rule.txt','cp936')
+    print rp.results()
--- paipan/ziwei.py
+++ paipan/ziwei.py
@@ -1,71 +1,82 @@
+import ruleparser
+
 class ZiWeiPaiPan(object):
-	def __init__(self):
-		super(ZiWeiPaiPan,self).__init__()
-		self._data={}
-		self._data['ShengNianGan']=10
-		self._data['ShengNianZhi']=6
-		self._data['ShengYueGan']=8
-		self._data['ShengYueZhi']=8
-		self._data['ShengRiGan']=6
-		self._data['ShengRiZhi']=8
-		self._data['ShengShiGan']=6
-		self._data['ShengShiZhi']=6
-		self._inputdata={}
-		self._outputdata={}
-		self._inputdata['ShengNianGan']=(1,2,3,4,5,6,7,8,9,10)
-		self._inputdata['ShengNianZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
-		self._inputdata['ShengYueGan']=(1,2,3,4,5,6,7,8,9,10)
-		self._inputdata['ShengYueZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
-		self._inputdata['ShengRiGan']=(1,2,3,4,5,6,7,8,9,10)
-		self._inputdata['ShengRiZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
-		self._inputdata['ShengShiGan']=(1,2,3,4,5,6,7,8,9,10)
-		self._inputdata['ShengShiZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
-		self.loadConfig('ziwei_rule.txt')
+    def __init__(self):
+        self.loadConfig('ziwei_rule.txt')
 
-	def loadConfig(self,cfile):
-		fh=open(cfile,'rU')
-		target=''
-		for line in fh.readlines():
-			line=line.strip()
-			if line.startswith('#'):
-				pass
-			elif line.startswith('Target:'):
-				if target!='':
-					self._inputdata[target]=tuple(int(x) for x in output.split(','))
-					self._outputdata[target]={'params':tuple(input.split(',')),'data':tuple(int(x) for x in data.split(','))}
-				target=line[7:]
-			elif line.startswith('Input:'):
-				input=line[6:]
-			elif line.startswith('Output:'):
-				output=line[7:]
-			elif line.startswith('Data:'):
-				data=''
-			else:
-				if data=='':
-					data=line
-				else:
-					if data.endswith(','):
-						data=data+line
-					else:
-						data=data+','+line
-		if target!='':
-			self._inputdata[target]=tuple(int(x) for x in output.split(','))
-			self._outputdata[target]={'params':tuple(input.split(',')),'data':tuple(int(x) for x in data.split(','))}
-		fh.close()
+    # public functions
+    def setParams(self,**kwargs):
+        self.init_data()
+        for k,v in kwargs.items():
+            self._data[k]=v
 
-	def getData(self,datakey,defaultvalue=''):
-		if datakey in self._inputdata:
-			if datakey in self._outputdata:
-				dindex=0
-				for pkey in self._outputdata[datakey]['params']:
-					dindex=dindex*len(self._inputdata[pkey])+self._inputdata[pkey].index(self.getData(pkey))
-				return self._outputdata[datakey]['data'][dindex]
-			else:
-				return self._data[datakey]
-		else:
-			return defaultvalue
+        for target in self._outputdata.keys():
+            self._data[target]=self.calcData(target)
+
+    def getData(self,datakey,defaultvalue=''):
+        if datakey in self._data:
+            return self._data[datakey]
+        else:
+            return defaultvalue
+
+    # private functions
+    def init_data(self):
+        self._data={}
+        self._data['ShengNianGan']=-1
+        self._data['ShengNianZhi']=-1
+        self._data['ShengYueGan']=-1
+        self._data['ShengYueZhi']=-1
+        self._data['ShengRiGan']=-1
+        self._data['ShengRiZhi']=-1
+        self._data['ShengShiGan']=-1
+        self._data['ShengShiZhi']=-1
+
+    def init_const(self):
+        self._inputdata={}
+        self._inputdata['ShengNianGan']=(1,2,3,4,5,6,7,8,9,10)
+        self._inputdata['ShengNianZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
+        self._inputdata['ShengYueGan']=(1,2,3,4,5,6,7,8,9,10)
+        self._inputdata['ShengYueZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
+        self._inputdata['ShengRiGan']=(1,2,3,4,5,6,7,8,9,10)
+        self._inputdata['ShengRiZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
+        self._inputdata['ShengShiGan']=(1,2,3,4,5,6,7,8,9,10)
+        self._inputdata['ShengShiZhi']=(1,2,3,4,5,6,7,8,9,10,11,12)
+        self._outputdata={}
+
+    def loadConfig(self,cfile,filecode='cp936'):
+        self.init_const()
+        rp=ruleparser.RuleParser()
+        rp.parse(cfile,filecode)
+        res=rp.results()
+        for r in res:
+            target=r['PTARGET']
+            self._inputdata[target]=tuple(r['POUTPUT'])
+            self._outputdata[target]={'name':r.get('PNAME',''),'params':tuple(r['PINPUT']),'data':tuple(r['PDATA'])}
+
+    def calcData(self,datakey,defaultvalue=''):
+        if datakey in self._inputdata:
+            if datakey in self._data:
+                return self._data[datakey]
+            elif datakey in self._outputdata:
+                dindex=0
+                for pkey in self._outputdata[datakey]['params']:
+                    dindex=dindex*len(self._inputdata[pkey])+self._inputdata[pkey].index(self.calcData(pkey))
+                return self._outputdata[datakey]['data'][dindex]
+            else:
+                return defaultvalue
+        else:
+            return defaultvalue
 
 if __name__=='__main__':
-	zw=ZiWeiPaiPan()
-	print zw._outputdata
-	print zw.getData('MingGong')
+    zw=ZiWeiPaiPan()
+    zw.setParams(
+        ShengNianGan=10,
+        ShengNianZhi=6,
+        ShengYueGan=8,
+        ShengYueZhi=8,
+        ShengRiGan=6,
+        ShengRiZhi=8,
+        ShengShiGan=6,
+        ShengShiZhi=6)
+    print zw._outputdata
+    print zw.getData('MingGong')
