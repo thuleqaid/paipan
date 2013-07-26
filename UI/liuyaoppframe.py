430beaa347b084030b92b63b18007d807de69815
Thule Qaid <thuleqaid@gmail.com>
2013-07-26 06:27:31 +0000

    v1.9 heluo paipan frame update

--- UI/liuyaoppframe.py
+++ UI/liuyaoppframe.py
@@ -18,9 +18,24 @@ class LiuyaoPPFrame(QFrame):
         if self.ui.radio_number.isChecked():
             info['method1']="%d,%d,%d"%(self.ui.edit_mtd11.text().toInt()[0], self.ui.edit_mtd12.text().toInt()[0], self.ui.edit_mtd13.text().toInt()[0])
         elif self.ui.radio_gua.isChecked():
-            info['method2']="%d"%(self.ui.edit_mtd2.text().toInt()[0])
+            guacode=[]
+            guacode.append(self.ui.combo_mtd21.currentIndex()+1)
+            guacode.append(self.ui.combo_mtd22.currentIndex()+1)
+            if self.ui.check_mtd21.isChecked():
+                guacode.append(1)
+            if self.ui.check_mtd22.isChecked():
+                guacode.append(2)
+            if self.ui.check_mtd23.isChecked():
+                guacode.append(3)
+            if self.ui.check_mtd24.isChecked():
+                guacode.append(4)
+            if self.ui.check_mtd25.isChecked():
+                guacode.append(5)
+            if self.ui.check_mtd26.isChecked():
+                guacode.append(6)
+            info['method2']=",".join([str(x) for x in guacode])
         else:
-            info['method3']="%d,%d,%d,%d,%d,%d"%(self.ui.edit_mtd31.text().toInt()[0], self.ui.edit_mtd32.text().toInt()[0], self.ui.edit_mtd33.text().toInt()[0], self.ui.edit_mtd34.text().toInt()[0], self.ui.edit_mtd35.text().toInt()[0], self.ui.edit_mtd36.text().toInt()[0])
+            info['method3']="%d,%d,%d,%d,%d,%d"%(self.ui.combo_mtd31.currentText().toInt()[0], self.ui.combo_mtd32.currentText().toInt()[0], self.ui.combo_mtd33.currentText().toInt()[0], self.ui.combo_mtd34.currentText().toInt()[0], self.ui.combo_mtd35.currentText().toInt()[0], self.ui.combo_mtd36.currentText().toInt()[0])
         d=self.ui.calendar.selectedDate()
         t=self.ui.time.time()
         info['datetime']=(d.year(),d.month(),d.day(),t.hour(),t.minute(),t.second())
@@ -35,23 +50,37 @@ class LiuyaoPPFrame(QFrame):
             self.ui.edit_mtd12.setEnabled(False)
             self.ui.edit_mtd13.setEnabled(False)
         if self.ui.radio_gua.isChecked():
-            self.ui.edit_mtd2.setEnabled(True)
+            self.ui.combo_mtd21.setEnabled(True)
+            self.ui.combo_mtd22.setEnabled(True)
+            self.ui.check_mtd21.setEnabled(True)
+            self.ui.check_mtd22.setEnabled(True)
+            self.ui.check_mtd23.setEnabled(True)
+            self.ui.check_mtd24.setEnabled(True)
+            self.ui.check_mtd25.setEnabled(True)
+            self.ui.check_mtd26.setEnabled(True)
         else:
-            self.ui.edit_mtd2.setEnabled(False)
+            self.ui.combo_mtd21.setEnabled(False)
+            self.ui.combo_mtd22.setEnabled(False)
+            self.ui.check_mtd21.setEnabled(False)
+            self.ui.check_mtd22.setEnabled(False)
+            self.ui.check_mtd23.setEnabled(False)
+            self.ui.check_mtd24.setEnabled(False)
+            self.ui.check_mtd25.setEnabled(False)
+            self.ui.check_mtd26.setEnabled(False)
         if self.ui.radio_coin.isChecked():
-            self.ui.edit_mtd31.setEnabled(True)
-            self.ui.edit_mtd32.setEnabled(True)
-            self.ui.edit_mtd33.setEnabled(True)
-            self.ui.edit_mtd34.setEnabled(True)
-            self.ui.edit_mtd35.setEnabled(True)
-            self.ui.edit_mtd36.setEnabled(True)
+            self.ui.combo_mtd31.setEnabled(True)
+            self.ui.combo_mtd32.setEnabled(True)
+            self.ui.combo_mtd33.setEnabled(True)
+            self.ui.combo_mtd34.setEnabled(True)
+            self.ui.combo_mtd35.setEnabled(True)
+            self.ui.combo_mtd36.setEnabled(True)
         else:
-            self.ui.edit_mtd31.setEnabled(False)
-            self.ui.edit_mtd32.setEnabled(False)
-            self.ui.edit_mtd33.setEnabled(False)
-            self.ui.edit_mtd34.setEnabled(False)
-            self.ui.edit_mtd35.setEnabled(False)
-            self.ui.edit_mtd36.setEnabled(False)
+            self.ui.combo_mtd31.setEnabled(False)
+            self.ui.combo_mtd32.setEnabled(False)
+            self.ui.combo_mtd33.setEnabled(False)
+            self.ui.combo_mtd34.setEnabled(False)
+            self.ui.combo_mtd35.setEnabled(False)
+            self.ui.combo_mtd36.setEnabled(False)
     def onBtnPaipan(self):
         if self._funcpp:
             self._funcpp(self)
